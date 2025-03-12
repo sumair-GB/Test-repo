a58a837cec1335752e9ef04873b235ae16dc7f18
sumair-GB <buttsumair@gmail.com>
2025-03-12 11:08:26 +0000

    Initial Files

---
+++ myPyton.py
@@ -0,0 +1,11 @@
+#write a program to print the number of seconds in a year
+# 1 minute = 60 seconds     
+# 1 hour = 60 minutes               
+# 1 day = 24 hours
+# 1 year = 365 days
+# 1 year = 365 * 24 * 60 * 60 seconds       
+
+print(365 * 24 * 60 * 60)
+
+
+
